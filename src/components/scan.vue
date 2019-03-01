03b24f4b406e0b55a440ca8329f38d431bf4fd0a
Talen Fisher <tqfisher@iastate.edu>
2019-03-01 09:49:09 -0600

    Squashed 'gui/' changes from d60d38b..d32cd58
    d32cd58 Merge remote-tracking branch 'origin/jenny' into HEAD
    bd17a49 scans page
    70a6f53 async issue fixed
    d7a7d78 merge 0deaeea
    44a66fb finished bullets/barrels page in set
    58f7f52 search bar
    c18564e scans page
    b60eb65 async issue fixed
    da6c2d8 temp bullet scans
    e6c2c66 finished bullets/barrels page in set
    df9a6cb search bar
    f55dd20 home.vue update
    
    git-subtree-dir: gui
    git-subtree-split: d32cd58b6d1dc36e8cc159fb53ec99fc89db9837

---
+++ src/components/scan.vue
@@ -0,0 +1,116 @@
+<template>
+    <main class="scan">
+        <div class="set-head">
+            <h1>{{ set.name }} - BULLET {{ bullet }}</h1>
+            <div class="row">
+                <div class="set-creationDate">Created on <time>{{ set.creationDate | format }}</time></div> / 
+                <div class="set-lastScanDate">Last updated: <time>{{ set.lastScanDate | format }}</time></div>
+            </div>
+        </div>
+        <ul class="set-barrels scan-barrels grid no-grow">
+                <li v-for="scan in scans" :key="scan.id">
+                    <router-link :to="{ name: '' }">
+                        <h3>Scan {{ scan.id }}</h3>
+                        <div class="counts">
+                            Barrel: <span> {{ scan.barrelNo }}</span> 
+                        </div>
+                         <div class="counts">
+                            Threshold: <span> {{ scan.threshold }}</span><br>
+                            Resolution: <span> {{ scan.resolution }}</span><br>
+                            Magnification: <span>{{ scan.magnification }}</span><br>
+                        </div>
+                    </router-link>
+                </li>
+        </ul>
+    </main>
+</template>
+
+<style lang="scss">
+    @import "../vars.scss";
+
+    h3 {
+        text-decoration: underline;
+    }
+    
+    .counts {
+        font-size: 1.2em;
+        font-weight: normal;
+        text-align: left;
+            
+        span {
+            font-weight: bold;
+            line-height: 20px;
+        }
+    }
+
+    .scan {
+        padding: 15px;
+
+        .set-head {
+            padding: 20px 0;
+            border-bottom: 5px solid $primaryColor;
+        }
+    }
+
+    .row {
+        div {
+            display: inline-block;
+            margin: 0 15px;
+
+            &:first-of-type {
+                margin-left: 0;
+            }
+        }
+    }    
+
+    .scan-barrels {
+        margin: 0 auto;
+        display: table;
+        li {
+            font-size: 10px;
+            text-align: center;
+            font-weight: bold;
+            line-height: 10px;
+            display: inline-block;
+            width: 200px;
+        }
+    }
+
+
+</style>
+
+<script lang="ts">
+    import Vue from "vue";
+    import Component from "vue-class-component";
+    declare var require: any
+    var _ = require('lodash');
+
+    @Component
+    export default class Scan extends Vue {
+        asyncData({ store, route }: DataParameters) {
+            return store.dispatch("getScans")
+        }
+
+        get scans() {
+            var tempscans = this.$store.state.scans;
+            var bNum = this.bullet;
+            var scanList = _.filter(tempscans, function(s) { 
+                return s.bulletNo == bNum; 
+            });
+            return scanList;
+        }
+
+        get set() {
+            return this.$store.state.sets[this.$route.params.name];
+        }
+
+        get sets() {
+            return this.$store.state.sets;
+        }
+
+        get bullet(){
+            return this.$route.params.number;
+        }
+
+    }
+</script>
